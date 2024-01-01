363c9a1dd99160138c4ffcda430cf3b2b279be2b
dhartiidudhat <dhartii.dudhat@thegatewaycorp.co.in>
2024-01-01 12:01:35 +0530

    03_1 FE & BE

--- second/FE/src/App.jsx
+++ second/FE/src/App.jsx
@@ -1,44 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 const App = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-  useEffect(()=>{
-    joke()
+  useEffect(() => {
+    joke();
     console.log(data);
+  }, []);
 
-  },[])
-
-  const joke = async () =>{
-    const res = await axios.get('http://localhost:4000/api/joke');
-    const data = await res
+  const joke = async () => {
+    const res = await axios.get("/api/joke");
+    const data = await res;
 
     console.log("data in ", data);
-    setData(data.data)
-  }
+    setData(data.data);
+  };
   return (
     <>
-    <h1>Full stack</h1>
-    <h4>joke Length :- </h4>
-    <h1>Joke</h1>
-    <h2>Content</h2>
+      <h1>Full stack</h1>
+      <h4>joke Length :- {data.length} </h4>
 
-    {
-      data.map((item, index) => (
+      {data.map((item, index) => {
         <>
-        <div key={index}>
-          <h1>name:- {item.name}</h1>
-          <h2>Content:- {item.content}</h2>
-        </div>
-        </>
-        
-      ))
-    }
-
+          <div key={index}>
+            <h1>name:- {item.name}</h1>
+            <h2>Content:- {item.content}</h2>
+          </div>
+        </>;
+      })}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
