1902b6693d6316ed7f8220e3cc54988dfd3069be
dhartiidudhat <dhartii.dudhat@thegatewaycorp.co.in>
2024-01-03 18:36:10 +0530

    Revert "utils & Error standard, middleware in app"
    This reverts commit 3dcc32ba408b4d702a78b3d317ffdbcdf8f03d86.

--- Jan1/Project/src/db/index.js
+++ Jan1/Project/src/db/index.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
-import { app } from "../app.js";
 
 const connectDB = async () => {
   try {
@@ -8,17 +7,10 @@ const connectDB = async () => {
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(`Db Connected Successfully !`, dbInstance.connection.host);
-    app.on("error", () => {
-      console.log("Express App not work !");
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App listen on port ${process.env.PORT}`);
-    });
   } catch (error) {
-    console.log("DB Connection error !", error);
+    console.log("DB Connection Failed !", error);
     process.exit(1);
   }
 };
 
-export default connectDB;
+export default connectDB
